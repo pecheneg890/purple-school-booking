041312b2d85f395225d0c924de087b1c70751d68
olaaveryanov <olaaveryanov@greenatom.ru>
2024-10-28 19:30:52 +0300

    Обновление по результатам комментариев

--- src/user/user.constants.ts
+++ src/user/user.constants.ts
@@ -1,2 +1,4 @@
 export const USER_NOT_FOUND = 'Пользователь не найден';
 export const USER_ALREADY_EXIST = 'Пользователь с данным email уже существует';
+export const HASH_ERR = 'Ошибка хеширования пароля';
+export const USER_CREATE_ERR = 'Ошибка создания пользователя';
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,7 +3,7 @@ import { User, UserDocument } from './schemas/user.schema';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { UserCreateDto } from './dto/user-create.dto';
-import { USER_ALREADY_EXIST } from './user.constants';
+import { HASH_ERR, USER_ALREADY_EXIST } from './user.constants';
 import { hash, genSalt } from 'bcryptjs';
 
 @Injectable()
@@ -11,16 +11,36 @@ export class UserService {
 	constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
 	async createUser(dto: UserCreateDto): Promise<UserDocument> {
+		let salt = '';
+		let passHash = '';
+
 		const oldUser = await this.findUser(dto.email);
 
 		if (oldUser) {
 			throw new BadRequestException(USER_ALREADY_EXIST);
 		}
 
-		const salt = await genSalt(10);
-		const newUser = dto;
-		newUser.password = await hash(newUser.password, salt);
-		return this.userModel.create(newUser);
+		try {
+			salt = await genSalt(10);
+			passHash = await hash(dto.password, salt);
+			// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		} catch (err) {
+			throw new BadRequestException(HASH_ERR);
+		}
+
+		const newUser: User = {
+			email: dto.email,
+			name: dto.name,
+			phone: dto.phone,
+			role: dto.role,
+			password: passHash,
+		};
+		try {
+			return await this.userModel.create(newUser);
+			// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		} catch (err) {
+			throw new BadRequestException(HASH_ERR);
+		}
 	}
 
 	async findUser(email: string): Promise<UserDocument | null> {
